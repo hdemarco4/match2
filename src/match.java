ccfbfc991748a0770598d74aa24947c04bd743c9
Heather DeMarco <hdemarco@msudenver.edu>
2017-10-04 18:53:52 -0600

    Initial commit

---
+++ src/match.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class match {
+
+    public static void main(String[] args){
+
+
+    }
+
+    Scanner s = new Scanner(System.in).useDelimiter("\\s");
+
+
+
+    Set pattern = s;
+    Set constant = s;
+    Set bindings = s;
+}
